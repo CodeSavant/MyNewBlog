baa75a61ce640123941a65690edc3c1678f41a85
CodeSavant <Bfiguereo20@gmail.com>
2016-01-08 17:33:27 -0500

    Added Transaction. Added basic user Admin.

--- SimpleBlog/App_Start/FilterConfig.cs
+++ SimpleBlog/App_Start/FilterConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.Infrastructure;
 
 
 namespace SimpleBlog
@@ -11,6 +12,7 @@ namespace SimpleBlog
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new TranactionFilter());
             filters.Add(new HandleErrorAttribute());
         }
     }
---
+++ SimpleBlog/Infrastructure/TranactionFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Infrastructure
+{
+
+    // global action filter. do not need to add attribute to action.
+    public class TranactionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Database.Session.BeginTransaction();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Exception == null)
+            {
+                Database.Session.Transaction.Commit();
+            }
+            else
+            {
+                Database.Session.Transaction.Rollback();
+            }
+        }
+    }
+}
